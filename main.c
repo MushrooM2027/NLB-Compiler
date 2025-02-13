0ce825ed3fb92a722f38cbc3c0797c94dc9b4d06
Aditya Prakash <126641422+MushrooM2027@users.noreply.github.com>
2025-02-13 23:17:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#define DS_IMPLEMENTATION
 #include "ds.h"
 #include <stdio.h>
 
@@ -65,8 +66,10 @@ void print_token(struct token *t){
     const char *type = token_type_names(t->type);
     if(t->literal != NULL){
         printf("Token: %s, Literal: %s\n", type, t->literal);
+    }else {
+        printf("Token: %s\n", type);
     }
-    printf("\n");
+    // printf("\n");
 
 }
 
@@ -114,6 +117,8 @@ static void lexer_init(struct lexer *l, char *buffer, unsigned int buffer_len) {
 static struct token lexer_next_token(struct lexer *l) {
     skip_whitespaces(l);
 
+    // printf("Current char: %c\n", l->ch);
+
     if(l->ch == EOF){
         lexer_read_char(l);
         return (struct token){.type = END, .literal = NULL};
@@ -155,40 +160,30 @@ static struct token lexer_next_token(struct lexer *l) {
         ds_string_slice slice = {.str = l->buffer + start, .len = l->pos - start};
         char *value = NULL;
         ds_string_slice_to_owned(&slice, &value);
-        if(strcmp(value, "input") == 0){
-            return (struct token){.type = INPUT, .literal = NULL};
-        }
-        else if(strcmp(value, "display") == 0){
-            return (struct token){.type = DISPLAY, .literal = NULL};
-        }
-        else if(strcmp(value, "set") == 0){
-            return (struct token){.type = SET, .literal = NULL};
-        }
-        else if(strcmp(value, "to") == 0){
-            return (struct token){.type = TO, .literal = NULL};
-        }
-        else if(strcmp(value,"if") == 0){
-            return (struct token){.type = IF, .literal = NULL};
-        }
-        else if(strcmp(value, "is greater than") == 0){
-            return (struct token){.type = IS_GREATER_THAN, .literal = NULL};
-        }
-        else if(strcmp(value, "then") == 0){
-            return (struct token){.type = THEN, .literal = NULL};
-        }
-        else if(strcmp(value, "otherwise") == 0){
-            return (struct token){.type = OTHERWISE, .literal = NULL};
-        }
-        else if(strcmp(value, "repeat") == 0){
-            return (struct token){.type = REPEAT, .literal = NULL};
-        }
-        else if(strcmp(value, "times") == 0){
-            return (struct token){.type = TIMES, .literal = NULL};
-        }
-        else{
-            return (struct token){.type = IDENTIFIER, .literal = value};
-        }
+        if (strcmp(value, "input") == 0) {
+        return (struct token){.type = INPUT, .literal = NULL}; 
+        } else if (strcmp(value, "display") == 0) {
+        return (struct token){.type = DISPLAY, .literal = NULL}; 
+        } else if (strcmp(value, "set") == 0) {
+        return (struct token){.type = SET, .literal = NULL}; 
+        } else if (strcmp(value, "to") == 0) {
+        return (struct token){.type = TO, .literal = NULL}; 
+        } else if (strcmp(value, "if") == 0) {
+        return (struct token){.type = IF, .literal = NULL}; 
+        } else if (strcmp(value, "is greater than") == 0) {
+        return (struct token){.type = IS_GREATER_THAN, .literal = NULL}; 
+        } else if (strcmp(value, "then") == 0) {
+        return (struct token){.type = THEN, .literal = NULL}; 
+        } else if (strcmp(value, "otherwise") == 0) {
+        return (struct token){.type = OTHERWISE, .literal = NULL}; 
+        } else if (strcmp(value, "repeat") == 0) {
+        return (struct token){.type = REPEAT, .literal = NULL}; 
+        } else if (strcmp(value, "times") == 0) {
+        return (struct token){.type = TIMES, .literal = NULL}; 
+        } else {
+        return (struct token){.type = IDENTIFIER, .literal = value};
     }
+}
     else {
         ds_string_slice slice = {.str = l->buffer + l->pos, .len = 1};
         char *value = NULL;
@@ -215,11 +210,18 @@ int lexer_tokenizer(char *buffer, unsigned int buffer_len,ds_dynamic_array *toke
     
 
 int main() {
-    char *buffer = NULL;
-    int length = ds_io_read_file(NULL, &buffer);
+    char *buffer = "input x = 5 + 5 define y = 10 * 10 display x display y";
+    int length = strlen(buffer);
 
     ds_dynamic_array tokens;
     ds_dynamic_array_init(&tokens, sizeof(struct token));
 
-    lexer_tokenize(buffer, length, &tokens);
+    lexer_tokenizer(buffer, length, &tokens);
+    for (unsigned int i=0; i < tokens.count; i++){
+        struct token *t = NULL;
+        ds_dynamic_array_get_ref(&tokens, i, (void **)&t);
+        print_token(t);
+    }
+
+    printf("Buffer: %s\n", buffer);
 }
